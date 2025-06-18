d82f08f34332c6c5298dce705f20fa077da65052
GeraldOKoth <okothgerald449@gmail.com>
2025-06-18 15:05:39 +0300

    Correct error in js file

--- quiz.js
+++ quiz.js
@@ -3,15 +3,14 @@ const submit = document.getElementById("submit-answer");
 function checkAnswer() {
   const correctAnswer = "4";
 
-  const userAnswer = document.querySelector('input[name="quiz"]:checked');
+  const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
   const feedback = document.getElementById("feedback");
 
   if (!userAnswer) {
     feedback.textContent = "Please select an answer.";
     return;
   }
-
-  if (userAnswer.value === correctAnswer) {
+  else if (userAnswer === correctAnswer) {
     feedback.textContent = "Correct! Well done.";
   } else {
     feedback.textContent = "That's incorrect. Try again!";
